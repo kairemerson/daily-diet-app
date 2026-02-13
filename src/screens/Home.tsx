4a1302ff5ed4c72ab8f2a9ea74526e67cf009dbf
Remerson Monteiro <kairemerson@gmail.com>
2026-02-13 20:55:26 -0300

    feat: add components at Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,55 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { useAuth } from "../contexts/AuthContext";
+import { PercentageCard } from "@/src/components/PercentageCard";
+import { Header } from "@/src/components/Header";
+import { MealItem } from "../components/MealItem";
+import { Button } from "../components/Button";
+import { AppStackParamList } from "../@types/navigation";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+
+export type NavigationProps = NativeStackNavigationProp<AppStackParamList>
+
 
 export function Home() {
   const { signOut } = useAuth();
 
+  const navigation = useNavigation<NavigationProps>()
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-xl font-bold mb-4">
-        Daily Diet Home
+    <ScrollView className="flex-1 bg-white px-6 pt-14">
+      
+      {/* Header */}
+      <View className="flex-row justify-between items-center mb-8">
+        <Text className="text-xl font-bold">
+          Daily Diet
+        </Text>
+
+        <View className="w-10 h-10 rounded-full bg-gray-300" />
+      </View>
+
+      {/* Percentage */}
+      <PercentageCard percentage={90} />
+
+      {/* Button */}
+      <Text className="text-gray-600 mb-2">
+        Refeições
       </Text>
 
-      <TouchableOpacity
-        className="bg-red-500 px-4 py-2 rounded-lg"
-        onPress={signOut}
-      >
-        <Text className="text-white">
-          Sair
-        </Text>
-      </TouchableOpacity>
-    </View>
+      <Button title="Nova Refeição" iconName="add" onPress={() => navigation.navigate("MealForm", {id: "123"})}/>
+
+      {/* List */}
+      <Header title="12.08.22" />
+
+      <MealItem time="20:00" title="X-tudo" isOnDiet={false} />
+      <MealItem time="16:00" title="Whey protein com leite" isOnDiet />
+      <MealItem time="12:30" title="Salada cesar com frango" isOnDiet />
+      <MealItem time="09:30" title="Vitamina de banana com..." isOnDiet />
+
+      <Header title="11.08.22" />
+
+      <MealItem time="20:00" title="X-tudo" isOnDiet={false} />
+      <MealItem time="16:00" title="Whey protein com leite" isOnDiet />
+    </ScrollView>
   );
 }
